e53a5b4e85df236dedc0ec03ff8e58f3f513a51d
Hoffe312 <hoffe312@googlemail.com>
2023-03-15 19:37:24 +0100

    Update API URL

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
-const express = require('express');
-const axios = require('axios');
-const { Telegraf } = require('telegraf');
-const main = require('./javascript.js');
+import express from "express";
+import axios from "axios";
+import { Telegraf } from "telegraf";
+import main from "./javascript.js";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/api/data', async (req, res) => {
-    try {
-        const url = 'https://pure-fortress-24810.herokuapp.com/'; // Ersetzen Sie dies durch die URL, die Sie abfragen möchten
-        const data = await main();
-        res.json({ result: data });
-    } catch (error) {
-        res.status(500).json({ error: 'Error fetching data' });
-    }
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/data", async (req, res) => {
+  try {
+    const data = await main();
+    res.json({ result: data });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error fetching data" });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at Heroku`);
+  console.log(`Server running on port ${port}`);
 });
 
-const botToken = '5963822653:AAGrHiIoFzIyvFZmgAN696Fy5336yVmODoI'; // Ersetzen Sie dies durch Ihren erhaltenen Bot-Token
+const botToken = "5963822653:AAGrHiIoFzIyvFZmgAN696Fy5336yVmODoI"; // Ersetzen Sie dies durch Ihren erhaltenen Bot-Token
 const bot = new Telegraf(botToken);
 
-bot.start((ctx) => ctx.reply('Willkommen beim NFT Champ Tool Bot!'));
-bot.help((ctx) => ctx.reply('Gib /data ein, um die neuesten Daten abzurufen.'));
+bot.start((ctx) => ctx.reply("Willkommen beim NFT Champ Tool Bot!"));
+bot.help((ctx) => ctx.reply("Gib /data ein, um die neuesten Daten abzurufen."));
 
-bot.command('data', async (ctx) => {
-    try {
-        const response = await axios.get('https://pure-fortress-24810.herokuapp.com/api/data');
-        ctx.reply(JSON.stringify(response.data.result, null, 2));
-    } catch (error) {
-        ctx.reply('Fehler beim Abrufen der Daten. Bitte versuchen Sie es später erneut.');
-    }
+bot.command("data", async (ctx) => {
+  try {
+    const response = await axios.get("https://pure-fortress-24810.herokuapp.com/api/data");
+    ctx.reply(JSON.stringify(response.data.result, null, 2));
+  } catch (error) {
+    ctx.reply("Fehler beim Abrufen der Daten. Bitte versuchen Sie es später erneut.");
+  }
 });
 
 bot.launch();
