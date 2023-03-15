0f5373c54014bbef48d16368fb13103409380157
Hoffe312 <hoffe312@googlemail.com>
2023-03-15 19:30:20 +0100

    Update API URL

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const axios = require('axios');
 const { Telegraf } = require('telegraf');
-const fetchData = require('./dataFetcher');
+const main = require('./javascript.js');
 
 const app = express();
 const port = 3000;
 
 app.get('/api/data', async (req, res) => {
     try {
-        const url = 'YOUR_URL'; // Ersetzen Sie dies durch die URL, die Sie abfragen möchten
-        const data = await fetchData(url);
+        const url = 'https://pure-fortress-24810.herokuapp.com/'; // Ersetzen Sie dies durch die URL, die Sie abfragen möchten
+        const data = await main();
         res.json({ result: data });
     } catch (error) {
         res.status(500).json({ error: 'Error fetching data' });
@@ -17,7 +17,7 @@ app.get('/api/data', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at Heroku`);
 });
 
 const botToken = '5963822653:AAGrHiIoFzIyvFZmgAN696Fy5336yVmODoI'; // Ersetzen Sie dies durch Ihren erhaltenen Bot-Token
@@ -28,7 +28,7 @@ bot.help((ctx) => ctx.reply('Gib /data ein, um die neuesten Daten abzurufen.'));
 
 bot.command('data', async (ctx) => {
     try {
-        const response = await axios.get('https://pure-fortress-24810.herokuapp.com/');
+        const response = await axios.get('https://pure-fortress-24810.herokuapp.com/api/data');
         ctx.reply(JSON.stringify(response.data.result, null, 2));
     } catch (error) {
         ctx.reply('Fehler beim Abrufen der Daten. Bitte versuchen Sie es später erneut.');
